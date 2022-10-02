bd290641f4de54615e473ac1168fed254ec7930e
Saul Lee <solleedata@gmail.com>
2022-10-02 12:06:31 +0900

    feat: apply react navigation, set common colors

--- App.tsx
+++ App.tsx
@@ -1,30 +1,43 @@
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   useColorScheme,
 } from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
+import {COLORS} from './app/styles/common';
 
 export default function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const Tab = createBottomTabNavigator();
+  const Stack = createNativeStackNavigator();
 
+  /**
+   * STYLES:
+   */
+  const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? COLORS.black : COLORS.floralwhite,
+  };
+  const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: COLORS.black,
+      background: COLORS.snow,
+    },
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}></ScrollView>
+      <NavigationContainer theme={MyTheme}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
---
+++ app/styles/common.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  white: '#ffffff',
+  snow: '#fffafa',
+  ivory: '#fffff0',
+  floralwhite: '#fffaf0',
+  gainsboro: '#dcdcdc',
+  lightgray: '#d3d3d3',
+  gray: '#808080',
+  slategray: '#708090',
+  salmon: '#fa8072',
+  tomato: '#ff6347',
+  midnightblue: '#191970',
+  black: '#000000',
+};

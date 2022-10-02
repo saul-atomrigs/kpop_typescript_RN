2634502517bddbc342e8952e4218c01410412a84
Saul Lee <solleedata@gmail.com>
2022-10-03 01:18:48 +0900

    feat: add new pages (react navigation)

--- App.tsx
+++ App.tsx
@@ -1,43 +1,89 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-} from 'react-native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {StatusBar, useColorScheme} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {CalendarPlus, ChatsCircle, Compass, User} from 'phosphor-react-native';
 import {COLORS} from './app/styles/common';
 
-export default function App() {
-  const Tab = createBottomTabNavigator();
-  const Stack = createNativeStackNavigator();
+/**
+ * IMPORT SCREENS:
+ */
+import Calendar from './app/components/view/calendar/Calendar';
+import Community from './app/components/view/community/Community';
+import Discover from './app/components/view/discover/Discover';
+import Me from './app/components/view/me/Me';
 
-  /**
-   * STYLES:
-   */
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? COLORS.black : COLORS.floralwhite,
-  };
-  const MyTheme = {
-    ...DefaultTheme,
-    colors: {
-      ...DefaultTheme.colors,
-      primary: COLORS.black,
-      background: COLORS.snow,
-    },
-  };
+  const backgroundColor = isDarkMode ? COLORS.black : COLORS.snow;
+  const textColor = isDarkMode ? COLORS.lightgray : COLORS.black;
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <NavigationContainer theme={MyTheme}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
+    <>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor={backgroundColor} />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="HomeTabNavigation" screenOptions={{headerShown: false}}>
+          <Stack.Screen name="HomeTabNavigation" component={HomeTabNavigation} />
+          <Stack.Screen name="Calendar" component={Calendar} />
+          <Stack.Screen name="Community" component={Community} />
+          <Stack.Screen name="Discover" component={Discover} />
+          <Stack.Screen name="Me" component={Me} />
+        </Stack.Navigator>
       </NavigationContainer>
-    </SafeAreaView>
+    </>
   );
 }
+
+function HomeTabNavigation() {
+  return (
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen name="Community" component={Community} />
+      <Tab.Screen
+        name="Calendar"
+        component={Calendar}
+        options={{
+          tabBarBadge: 'new',
+          tabBarBadgeStyle: {backgroundColor: 'pink'},
+        }}
+      />
+      <Tab.Screen name="Discover" component={Discover} />
+      <Tab.Screen name="Me" component={Me} />
+      {/* <Tab.Screen name="Korean" component={Korean} options={{ tabBarBadge: 2, tabBarBadgeStyle: { backgroundColor: 'pink' } }} /> */}
+    </Tab.Navigator>
+  );
+}
+
+/**
+ * TAB BAR ICONS
+ * @param {string} route icon
+ * return bottom tab bar icons
+ */
+const screenOptions = ({route}) => ({
+  tabBarIcon: ({focused, color, size}) => {
+    let iconSource;
+    if (route.name === 'Community') {
+      iconSource = focused ? <ChatsCircle weight="fill" size={30} /> : <ChatsCircle weight="duotone" size={25} />;
+    } else if (route.name === 'Calendar') {
+      iconSource = focused ? <CalendarPlus weight="fill" size={30} /> : <CalendarPlus weight="duotone" size={25} />;
+    } else if (route.name === 'Me') {
+      iconSource = focused ? <User weight="duotone" size={30} /> : <User weight="regular" size={25} />;
+    } else if (route.name === 'Discover') {
+      iconSource = focused ? <Compass weight="fill" size={30} /> : <Compass size={25} />;
+    }
+    return iconSource;
+  },
+  tabBarActiveTintColor: 'black',
+  tabBarInactiveTintColor: 'gray',
+  tabBarActiveBackgroundColor: '#eaf1f8',
+  tabBarItemStyle: {
+    borderRadius: 5,
+    paddingBottom: 5,
+    paddingTop: 5,
+  },
+  tabBarStyle: {position: 'absolute'},
+  tabBarLabelPosition: 'below-icon',
+});
---
+++ app/components/view/calendar/Calendar.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {COLORS, STYLES} from '../../../styles/common';
+
+export default function Calendar() {
+  return (
+    <View style={STYLES.app}>
+      <Text>Calendar</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});

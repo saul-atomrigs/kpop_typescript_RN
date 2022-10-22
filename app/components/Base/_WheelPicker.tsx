7dd1c2a7161abfead8f844ef2f9024b4542a48b4
Saul Lee <solleedata@gmail.com>
2022-10-22 09:47:34 +0900

    refactor: Create artists list util file

--- app/components/Base/_WheelPicker.tsx
+++ app/components/Base/_WheelPicker.tsx
@@ -1,35 +1,12 @@
-import {Picker} from '@react-native-picker/picker';
 import React from 'react';
+import {Picker} from '@react-native-picker/picker';
+import {artists} from '../../utils/artists';
 
 interface PropTypes {
   selectedValue: string;
   onValueChange: () => void;
 }
 
-const artists = [
-  '',
-  'NewJeans',
-  'aespa',
-  'Apink',
-  'ASTRO',
-  'ATEEZ',
-  'BLACKPINK',
-  'BTS',
-  'Cherry Bullet',
-  'STAYC',
-  'Twice',
-  'EXO',
-  'NCT 127',
-  'NMIXX',
-  'Stray Kids',
-  'ITZY',
-  'TXT',
-  'Kep1er',
-  'IVE',
-  'VIVIZ',
-].sort(function (a, b) {
-  return a.toLowerCase().localeCompare(b.toLowerCase());
-});
 export default function _WheelPicker({selectedValue, onValueChange}: PropTypes) {
   return (
     <Picker selectedValue={selectedValue} onValueChange={onValueChange}>
---
+++ app/utils/artists.ts
@@ -0,0 +1,24 @@
+export const artists = [
+  '',
+  'NewJeans',
+  'aespa',
+  'Apink',
+  'ASTRO',
+  'ATEEZ',
+  'BLACKPINK',
+  'BTS',
+  'Cherry Bullet',
+  'STAYC',
+  'Twice',
+  'EXO',
+  'NCT 127',
+  'NMIXX',
+  'Stray Kids',
+  'ITZY',
+  'TXT',
+  'Kep1er',
+  'IVE',
+  'VIVIZ',
+].sort(function (a, b) {
+  return a.toLowerCase().localeCompare(b.toLowerCase());
+});

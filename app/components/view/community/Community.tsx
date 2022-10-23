2610504c4a2c2fc2105f497a0cd03c1e84e89d6a
Saul Lee <solleedata@gmail.com>
2022-10-23 15:41:35 +0900

    feat: set up Community feeds component (title,date

--- app/components/view/community/Community.tsx
+++ app/components/view/community/Community.tsx
@@ -1,15 +1,60 @@
-import {StyleSheet, Text, Pressable} from 'react-native';
 import React, {useEffect, useState} from 'react';
-import Amplify, {API, graphqlOperation} from 'aws-amplify';
-import config from '../../../../backend/aws-exports';
-import {listEvents} from '../../../../backend/graphql/queries';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import styled from '@emotion/native';
 
-export default function Community() {
+import {App, ScrollView, VerticalContainer as FeedContainer, BoldText, RegularText} from '../../../styles/common';
+
+import {API, graphqlOperation} from 'aws-amplify';
+import {listPosts} from '../../../../backend/graphql/queries';
+import {textReducer} from '../../../utils/functions';
+
+export default function Community({navigation}) {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    try {
+      const postData = await API.graphql(graphqlOperation(listPosts));
+      setPosts(postData.data.listPosts.items);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const titleReduced = title => {
+    return textReducer(title, 80, dots);
+  };
+
+  const createdAtReduced = createdAt => {
+    return textReducer(createdAt, 10, dots === false);
+  };
+
+  const orderInDate = (a, b) => new Date(b.createdAt) - new Date(a.createdAt);
+
+  /**
+   * COMPONENTS:
+   */
+  const Feed = ({title, createdAt}) => (
+    <FeedContainer onPress={() => {}}>
+      <BoldText>{titleReduced(title)}</BoldText>
+      <Footer>
+        <RegularText>{createdAtReduced(createdAt)}</RegularText>
+      </Footer>
+    </FeedContainer>
+  );
+
   return (
-    <Pressable>
-      <Text>Community</Text>
-    </Pressable>
+    <App>
+      <FlatList data={posts.sort(orderInDate)} renderItem={({item}) => <Feed title={item.title} createdAt={item.createdAt} />} />
+    </App>
   );
 }
 
 const styles = StyleSheet.create({});
+
+const Footer = styled.View`
+  padding-top: 10px;
+`;

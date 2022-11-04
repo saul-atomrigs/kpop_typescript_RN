42639b8dfc13898421b663ca46efaad8ebd874f3
Saul Lee <solleedata@gmail.com>
2022-11-04 13:00:57 +0900

    fix: Remove auth initializing

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,6 +1,4 @@
 import auth from '@react-native-firebase/auth';
-import {appleAuth} from '@invertase/react-native-apple-authentication';
-import {LoginManager, AccessToken} from 'react-native-fbsdk-next';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 export const signOut = () => {
@@ -26,26 +24,6 @@ export const signInWithEmailAndPassword = (email: string, password: string) => {
 
       console.error(error);
     });
-};
-
-export const onAppleButtonPress = async () => {
-  // Start the sign-in request
-  const appleAuthRequestResponse = await appleAuth.performRequest({
-    requestedOperation: appleAuth.Operation.LOGIN,
-    requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
-  });
-
-  // Ensure Apple returned a user identityToken
-  if (!appleAuthRequestResponse.identityToken) {
-    throw new Error('Apple Sign-In failed - no identify token returned');
-  }
-
-  // Create a Firebase credential from the response
-  const {identityToken, nonce} = appleAuthRequestResponse;
-  const appleCredential = auth.AppleAuthProvider.credential(identityToken, nonce);
-
-  // Sign the user in with the credential
-  return auth().signInWithCredential(appleCredential);
 };
 
 export const onGoogleButtonPress = async () => {
--- index.js
+++ index.js
@@ -10,29 +10,4 @@ import {Amplify} from 'aws-amplify';
 import awsconfig from './backend/aws-exports';
 Amplify.configure(awsconfig);
 
-import {GoogleSignin} from '@react-native-google-signin/google-signin';
-
-import firebase from '@react-native-firebase/app';
-
-const RNfirebaseConfig = {
-  apiKey: 'AIzaSyCz-Wd5fT_9DNmaJp_mGBYu9NjRSgBLk3U',
-  authDomain: 'note-app-rn.firebaseapp.com',
-  projectId: 'note-app-rn',
-  storageBucket: 'note-app-rn.appspot.com',
-  // messagingSenderId: ".....",
-  appId: '1:634344250588:web:6922ef76271f0f4a58e819',
-};
-
-// if (firebase.apps.length === 0) {
-// app = firebase.initializeApp(RNfirebaseConfig);
-firebase.initializeApp(RNfirebaseConfig);
-// }
-
-export const auth = firebase.auth();
-
-GoogleSignin.configure({
-  // webClientId: '410819928050-r0q6jqltshqv8ji8hh6m6lmejfd3nmot.apps.googleusercontent.com',
-  webClientId: '634344250588-lgu10halk6fqqu366rdnommu47pekmc4.apps.googleusercontent.com',
-});
-
 AppRegistry.registerComponent(appName, () => App);

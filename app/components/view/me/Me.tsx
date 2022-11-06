24628e6135a9ff103bee2e49fa6416171d31367c
Saul Lee <solleedata@gmail.com>
2022-11-06 16:26:15 +0900

    refactor: Remove firebase related code

--- app/components/view/me/Me.tsx
+++ app/components/view/me/Me.tsx
@@ -1,16 +1,33 @@
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {Pressable, NativeModules, Text, View, Button} from 'react-native';
 import React from 'react';
-import {onGoogleButtonPress, signOut} from '../../../utils/auth';
+// import {onGoogleButtonPress, signOut} from '../../../utils/auth';
 
 export default function Me() {
+  const {CalendarModule} = NativeModules;
+
+  const NewModuleButton = () => {
+    const onPress = () => {
+      CalendarModule.createCalendarEvent('testName', 'testLocation');
+      // AlertModule.alert('Hello Module!');
+      // console.log(AlertModule.NUMBER_VALUE);
+      // console.log('We will invoke the native module here!');
+    };
+
+    return <Button title="Click to invoke your native module!" color="#841584" onPress={onPress} />;
+  };
+
+  // const {DEFAULT_EVENT_NAME} = CalendarModule.getConstants();
+  // console.log(DEFAULT_EVENT_NAME);
+
   return (
     <View>
-      <Pressable onPress={onGoogleButtonPress}>
+      {/* <Pressable onPress={onGoogleButtonPress}>
         <Text>Google login</Text>
-      </Pressable>
-      <Pressable onPress={signOut}>
+      </Pressable> */}
+      <NewModuleButton />
+      {/* <Pressable onPress={signOut}>
         <Text>Google log out</Text>
-      </Pressable>
+      </Pressable> */}
     </View>
   );
 }
--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,40 +1,40 @@
-import auth from '@react-native-firebase/auth';
-import {GoogleSignin} from '@react-native-google-signin/google-signin';
+// import auth from '@react-native-firebase/auth';
+// import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
-export const signOut = () => {
-  auth()
-    .signOut()
-    .then(() => console.log('User signed out!'));
-};
+// export const signOut = () => {
+//   auth()
+//     .signOut()
+//     .then(() => console.log('User signed out!'));
+// };
 
-export const signInWithEmailAndPassword = (email: string, password: string) => {
-  auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then(() => {
-      console.log('User account created & signed in!');
-    })
-    .catch(error => {
-      if (error.code === 'auth/email-already-in-use') {
-        console.log('That email address is already in use!');
-      }
+// export const signInWithEmailAndPassword = (email: string, password: string) => {
+//   auth()
+//     .createUserWithEmailAndPassword(email, password)
+//     .then(() => {
+//       console.log('User account created & signed in!');
+//     })
+//     .catch(error => {
+//       if (error.code === 'auth/email-already-in-use') {
+//         console.log('That email address is already in use!');
+//       }
 
-      if (error.code === 'auth/invalid-email') {
-        console.log('That email address is invalid!');
-      }
+//       if (error.code === 'auth/invalid-email') {
+//         console.log('That email address is invalid!');
+//       }
 
-      console.error(error);
-    });
-};
+//       console.error(error);
+//     });
+// };
 
-export const onGoogleButtonPress = async () => {
-  // Check if your device supports Google Play
-  await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
-  // Get the users ID token
-  const {idToken} = await GoogleSignin.signIn();
+// export const onGoogleButtonPress = async () => {
+//   // Check if your device supports Google Play
+//   await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+//   // Get the users ID token
+//   const {idToken} = await GoogleSignin.signIn();
 
-  // Create a Google credential with the token
-  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+//   // Create a Google credential with the token
+//   const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
-  // Sign-in the user with the credential
-  return auth().signInWithCredential(googleCredential);
-};
+//   // Sign-in the user with the credential
+//   return auth().signInWithCredential(googleCredential);
+// };
